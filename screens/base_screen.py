4473fc22aa4cb1de2c05aceb618e6d08db1b0e2e
texcoaster <phajun105@naver.com>
2026-02-01 17:25:45 +0900

    feat: Add minor enhancements to BaseScreen and Button classes (#2, #3)

--- screens/base_screen.py
+++ screens/base_screen.py
@@ -5,6 +5,10 @@ class BaseScreen(ABC):
     def __init__(self, game_controller):
         self.game_controller = game_controller
 
+    def set_game_controller(self, game_controller):
+        """Sets or updates the game controller for this screen."""
+        self.game_controller = game_controller
+
     @abstractmethod
     def handle_events(self, events):
         pass
